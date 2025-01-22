8b2342b985cca422a32eb7a00b93daec473cd2ae
s2272333 <murphy@sms.ed.ac.uk>
2025-01-22 21:50:22 +0000

    #2 failing test for queue initialisation

---
+++ test/test_queue.c
@@ -0,0 +1,9 @@
+#include "../src/queue.h"
+#include "../unity/unity.h"
+
+//Test 1
+//Test if queue structure exists and has memory allocated to it 
+void test_queue_exists_isFull(void){
+    Queue q = init_queue(&q, 10);
+    TEST_ASSERT_NOT_NULL(q.data);
+}
